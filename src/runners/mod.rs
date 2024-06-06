2c66ecd8774935543b091ffd93758b4587428942
Ali Ghahremani <alighahremani1377@gmail.com>
2024-06-06 10:44:26 +0330

    fix: wasm32 lib build by excluding repl and file_executor

--- src/runners/mod.rs
+++ src/runners/mod.rs
@@ -1,3 +1,10 @@
+// evel should be in bin and lib (file executor is using eval BTW)
 pub mod eval;
+
+// file executor should not include in wasm (we can't read/write files on browsers)
+#[cfg(not(target_family = "wasm"))]
 pub mod file_executor;
+
+// repl should not include in wasm (rustyline can not compile to wasm)
+#[cfg(not(target_family = "wasm"))]
 pub mod repl;
